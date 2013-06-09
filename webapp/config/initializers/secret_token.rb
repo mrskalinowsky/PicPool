25216f4eeca68a97df07f306d887c4ec5cb2b15c
Christian Smith <christians@datacom.co.nz>
2013-06-10 09:17:51 +1200

    Initial Project

---
+++ webapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picpool::Application.config.secret_token = 'e72857dcf856ab5c87c9768a4e282c9ebb7d75d4b1b55f421842c647b83597c46b20cd94ebd6623c61ecfcd241dc523e694e351d1b570256d3c349466a821eb8'
