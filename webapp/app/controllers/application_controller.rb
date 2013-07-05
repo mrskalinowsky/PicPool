2c68d3216898a544150d74b7d0b3ace50683280c
mshearer123 <msshearer123@gmail.com>
2013-07-05 11:36:44 +0100

    route to pools on confirm of email

--- webapp/app/controllers/application_controller.rb
+++ webapp/app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def after_sign_in_path_for(resource_or_scope)
-    albums_home_path
+    #debugger
+    user_pools_path(resource_or_scope)
+    #albums_home_path
   end
   
   def after_sign_out_path_for(resource_or_scope)
--- webapp/config/environments/development.rb
+++ webapp/config/environments/development.rb
@@ -38,4 +38,8 @@ Picpool::Application.configure do
   # Devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  # mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
 end
