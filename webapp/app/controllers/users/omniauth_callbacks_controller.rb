60d98435c5f42e745cef4fdc2e3dab32b22c7d6c
mshearer123 <mshearer123@gmail.com>
2013-07-06 11:58:34 +0100

    remove debugs

--- webapp/app/controllers/users/omniauth_callbacks_controller.rb
+++ webapp/app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,12 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
     if @user.persisted?
-      debugger
+      #debugger
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       #redirect_to user_pools_path(@user), :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      debugger
+      #debugger
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
